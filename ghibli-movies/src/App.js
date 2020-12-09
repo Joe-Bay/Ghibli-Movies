734c2c7b1850c39ebab50627d67e999e76bdb9df
Joe Bay <jbay55278@gmail.com>
2020-12-09 12:07:28 -0900

    made the CRA and displayed basic data from the api

---
+++ ghibli-movies/src/App.js
@@ -0,0 +1,14 @@
+import FilmList from './components/filmList'
+import Film from './components/film'
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <FilmList/>
+      <Film />
+    </div>
+  );
+}
+
+export default App;
---
+++ ghibli-movies/src/components/film.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import axios from 'axios'
+
+const Film = () => {
+    return (
+        <div>
+            <h2>Film</h2>
+        </div>
+    )
+}
+export default Film
